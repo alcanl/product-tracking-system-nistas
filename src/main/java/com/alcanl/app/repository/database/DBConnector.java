e67d74247f577fa5e7ba6cd88453694e0129f39a
Alican Keçici <alican.kecici@gmail.com>
2023-12-18 19:27:30 +0300

    18.12.2023 | Service layer process started

--- src/main/java/com/alcanl/app/repository/database/DBConnector.java
+++ src/main/java/com/alcanl/app/repository/database/DBConnector.java
@@ -4,26 +4,55 @@ import static com.alcanl.app.global.Resources.*;
 
 import com.alcanl.app.repository.RepositoryException;
 import com.alcanl.app.repository.entity.Material;
-import com.karandev.util.console.Console;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.concurrent.*;
 
 public final class DBConnector {
+    private final static ExecutorService threadPool = Executors.newSingleThreadExecutor();
     private DBConnector() {}
+    private static boolean saveNewDataCallback(Material material)
+    {
+        try (Connection connection = DriverManager.getConnection(DB_URL)) {
+
+            var statement = connection.prepareStatement(ADD_NEW_DATA);
+
+            statement.setString(1, material.getName());
+            statement.setDouble(2, material.getRadius().orElse(0.0));
+            statement.setString(3, material.getLength().orElse(null));
+            statement.setDouble(4, material.getUnitPrice().doubleValue());
+
+            statement.executeUpdate();
+
+            return true;
+        }
+        catch(SQLException ex)
+        {
+            throw new RepositoryException(ex.getCause());
+        }
+    }
+    private static void selectAllQueryCallBack(ResultSet resultSet, ArrayList<Material> arrayList)
+    {
+        try {
+            while (resultSet.next()) {
+                var material = new Material(resultSet.getInt(COLUMN_ID),
+                        resultSet.getString(COLUMN_NAME),
+                        resultSet.getDouble(COLUMN_RADIUS),
+                        resultSet.getString(COLUMN_LENGTH),
+                        resultSet.getDouble(COLUMN_UNIT_PRICE));
+
+                arrayList.add(material);
+            }
+        } catch (SQLException ex) {
+            throw new RepositoryException(ex.getCause());
+        }
+    }
     private static ArrayList<Material> runSelectAllQuery(Connection connection) throws SQLException {
 
         var arrayList = new ArrayList<Material>();
         var resultSet = connection.createStatement().executeQuery(SELECT_ALL_DATA);
+        threadPool.execute(() -> selectAllQueryCallBack(resultSet, arrayList));
 
-        while (resultSet.next()) {
-            var material = new Material(resultSet.getInt(COLUMN_ID),
-                    resultSet.getString(COLUMN_NAME),
-                    resultSet.getDouble(COLUMN_RADIUS),
-                    resultSet.getString(COLUMN_LENGTH),
-                    resultSet.getDouble(COLUMN_UNIT_PRICE));
-
-            arrayList.add(material);
-        }
         return arrayList;
     }
     private static boolean updateDoubleData(int id, String command, double newValue)
@@ -72,7 +101,7 @@ public final class DBConnector {
             throw new RepositoryException(ex.getCause());
         }
     }
-    public static ArrayList<Material> getWholeData()
+    public static ArrayList<Material> getAllData()
     {
         try (Connection connection = DriverManager.getConnection(DB_URL)){
 
@@ -81,31 +110,15 @@ public final class DBConnector {
         }
         catch (SQLException ex)
         {
-            Console.writeLine(ex.getMessage());
-            return null;
-        }
-
-    }
-    public static boolean saveData(Material material)
-    {
-        try (Connection connection = DriverManager.getConnection(DB_URL)) {
-
-            var statement = connection.prepareStatement(ADD_NEW_DATA);
-
-            statement.setString(1, material.getName());
-            statement.setDouble(2, material.getRadius().orElse(0.0));
-            statement.setString(3, material.getLength().orElse(null));
-            statement.setDouble(4, material.getUnitPrice().doubleValue());
-
-            statement.executeUpdate();
-
-            return true;
-        }
-        catch(SQLException ex)
-        {
             throw new RepositoryException(ex.getCause());
         }
+
     }
+    public static void saveNewData(Material material)
+    {
+        threadPool.execute(() -> saveNewDataCallback(material));
+    }
+
     public static boolean updateDataName(int id, String newName)
     {
         return updateStringData(id, UPDATE_COLUMN_NAME, newName);
@@ -123,7 +136,7 @@ public final class DBConnector {
         return updateDoubleData(id, UPDATE_COLUMN_UNIT_PRICE, newUnitPrice);
     }
     //increases all data prices as ratio value
-    public static boolean updateAllDataUnitPrice(double ratio)
+    public static boolean updateAllDataUnitPrices(double ratio)
     {
         return updateDoubleData(0, UPDATE_ALL_UNIT_PRICES, ratio);
     }
---
+++ src/main/java/com/alcanl/app/service/ApplicationService.java
@@ -0,0 +1,35 @@
+package com.alcanl.app.service;
+
+import com.alcanl.app.repository.database.DBConnector;
+import com.alcanl.app.repository.entity.Material;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ApplicationService {
+    private boolean isDataUpdated;
+    private ArrayList<Material> materials;
+
+    public ApplicationService()
+    {
+        materials = DBConnector.getAllData();
+    }
+    public void setUpdated(boolean isUpdated)
+    {
+        isDataUpdated = isUpdated;
+    }
+    public boolean getUpdated()
+    {
+        return isDataUpdated;
+    }
+    public List<Material> searchByName(String name)
+    {
+        return materials.stream().filter(m -> m.getName().equals(name)).toList();
+    }
+    public List<Material> searchByLength(String length)
+    {
+        return materials.stream().filter(m -> m.getLength().equals(length)).toList();
+    }
+}
